b0fbe697c77a75e2af28d306cfc94c9333e1e397
Zia <ziakayani.official@gmail.com>
2025-09-10 12:42:08 -0700

    add security group list and ami map

--- proj_with_variables/createinstance.tf
+++ proj_with_variables/createinstance.tf
@@ -9,9 +9,15 @@ terraform {
 
 
 resource "aws_instance" "my_instance" {
-  ami = "ami-0b0ea68c435eb488d"
+  # ami = "ami-0b0ea68c435eb488d"
+  ami = "${lookup(var.ami_list , var.AwsDefaultRegion)}"
   instance_type = "t2.micro"
   tags = {
     Name="zee_instance"
   }
+
+  #adding security group
+  security_groups = "${var.security_group}"
 }
+
+
--- proj_with_variables/variables.tf
+++ proj_with_variables/variables.tf
@@ -6,3 +6,20 @@ variable "AwsSecretAccessKey" {
 
 variable "AwsDefaultRegion" {
 }
+
+#for securoty group
+variable "security_group" {
+  type = list 
+  default = ["sg-1234", "sg-3455", "sg-5555"]
+}
+
+#for map 
+variable "ami_list" {
+  type = map 
+  default = {
+    us-east-1 = "ami-0b0ea68c435eb488d"
+    us-east-2 = "	ami-05803413c51f242b7"
+    us-west-1	= "ami-0454207e5367abf01"
+  }
+  
+}
