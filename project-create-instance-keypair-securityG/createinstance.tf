508fca0fdf0fe8781b7a4f0fe4cb8ef1a8ab477b
Zia <ziakayani.official@gmail.com>
2025-09-11 12:33:38 -0700

    adding userdata commands and run that with creating the aws instance

--- project-create-instance-keypair-securityG/createinstance.tf
+++ project-create-instance-keypair-securityG/createinstance.tf
@@ -13,7 +13,8 @@ resource "aws_instance" "my_instance" {
   instance_type = var.AwsInstanceType
   key_name = aws_key_pair.zia_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.sg_tf.id]
-
+  
+  user_data = file("${path.module}/script.sh")
 
   tags = {
     Name="zee_instance"
--- project-create-instance-keypair-securityG/terraform.tfvars
+++ project-create-instance-keypair-securityG/terraform.tfvars
@@ -1,4 +1,4 @@
 AwsDefaultRegion = "us-east-1"
 ports = [22,3306,443,80]
-AwsAmi = "ami-0b0ea68c435eb488d"
+AwsAmi = "ami-090c309e8ced8ecc2"
 AwsInstanceType = "t2.micro"
